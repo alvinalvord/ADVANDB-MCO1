70fa928f5a7e23dd4c9fefb9ae246a1fcc1bdf1d
Norielle Aguila <norielle_aguila@dlsu.edu.ph>
2017-10-23 01:47:11 +0800

    query3 and 5

--- QueryApp/src/model/Query3.java
+++ QueryApp/src/model/Query3.java
@@ -7,24 +7,89 @@ public class Query3 extends QueryObject {
 	public Query3 () {
 		super ();
 		initVariants ();
-		table = new Table ("Title", "PublisherName");
+		table = new Table ("BookID", "Title", "AuthorLastName");
 		setViewing (0);
 	}
 	
 	private void initVariants () {
 		variants.add
-			("select title, publishername " +
-			"from book " +
-			"where publishername like '%book%'");
-			variants.add
-			("select b.title, b.publishername " +
-			"from (select title, publishername from book where publishername like '%book%') as b");
+			("SELECT b.BookID, b.Title, ba.AuthorLastName "
+			+ "FROM book b, book_authors ba "
+			+ "WHERE b.BookID = ba.BookID AND " 
+			+ "ba.AuthorLastName = 'Twodream'");
+		variants.add
+		("SELECT b.BookID, b.Title, ba.AuthorLastName "
+		+ "FROM book b, (SELECT BookID, AuthorLastName FROM book_authors WHERE AuthorLastName  "
+		+ "= 'Twodream') as ba " 
+		+ "WHERE b.BookID = ba.BookID");
+		variants.add
+		("SELECT b.BookID, b.Title, ba.AuthorLastName "
+		+ "FROM book b, book_authors ba "
+		+ "WHERE b.BookID = ba.BookID AND " 
+		+ "ba.AuthorLastName = 'Twodream'");
+		variants.add
+		("SELECT b.BookID, b.Title, ba.AuthorLastName "
+		+ "FROM book b, book_authors ba "
+		+ "WHERE b.BookID = ba.BookID AND " 
+		+ "ba.AuthorLastName = 'Twodream'");
+		variants.add
+		("SELECT b.BookID, b.Title, ba.AuthorLastName "
+		+ "FROM book b, book_authors ba "
+		+ "WHERE b.BookID = ba.BookID AND " 
+		+ "ba.AuthorLastName = 'Twodream'");
+		variants.add
+		("SELECT b.BookID, b.Title, ba.AuthorLastName "
+		+ "FROM book b, (SELECT BookID, AuthorLastName FROM book_authors WHERE AuthorLastName  "
+		+ "= 'Twodream') as ba " 
+		+ "WHERE b.BookID = ba.BookID");
+		variants.add
+		("SELECT b.BookID, b.Title, ba.AuthorLastName "
+		+ "FROM book b, (SELECT BookID, AuthorLastName FROM book_authors WHERE AuthorLastName  "
+		+ "= 'Twodream') as ba " 
+		+ "WHERE b.BookID = ba.BookID");
+		variants.add
+		("SELECT b.BookID, b.Title, ba.AuthorLastName "
+		+ "FROM book b, (SELECT BookID, AuthorLastName FROM book_authors WHERE AuthorLastName  "
+		+ "= 'Twodream') as ba " 
+		+ "WHERE b.BookID = ba.BookID");
 	}
 	
 	public void prepareUpdates () throws Exception {
 		table.removeAllRowItems ();
 		
 		setQuery(variants.get(viewing));
+		
+		try{dbc.executeUpdate("drop index a on book");}catch(Exception e){}
+		try{dbc.executeUpdate("drop index b on book_authors");}catch(Exception e){}
+		
+		switch(viewing){
+		case 2:
+			dbc.executeUpdate("create index a on book(title)");
+			setQuery("create index a on book(title);\n" + getQuery());
+			break;
+		case 3:
+			dbc.executeUpdate("create index b on book_authors(AuthorLastName)");
+			setQuery("create index a on book(title);\n" + getQuery());
+			break;
+		case 4:
+			dbc.executeUpdate("create index a on book(title)");
+			dbc.executeUpdate("create index b on book_authors(AuthorLastName)");
+			setQuery("create index a on book(title);\n" + getQuery());
+			break;
+		case 5:
+			dbc.executeUpdate("create index a on book(title)");
+			setQuery("create index a on book(title);\n" + getQuery());
+			break;
+		case 6:
+			dbc.executeUpdate("create index b on book_authors(AuthorLastName)");
+			setQuery("create index a on book(title);\n" + getQuery());
+			break;
+		case 7:
+			dbc.executeUpdate("create index a on book(title)");
+			dbc.executeUpdate("create index b on book_authors(AuthorLastName)");
+			setQuery("create index a on book(title);\n" + getQuery());
+			break;
+		}
 		
 		long startTime, endTime;
 		startTime = System.nanoTime ();
@@ -34,7 +99,7 @@ public class Query3 extends QueryObject {
 		setDuration ((endTime - startTime));
 		
 		while (rs.next ()) {
-			table.addRowItem (new RowItem (rs.getString (1), rs.getString (2)));
+			table.addRowItem (new RowItem (rs.getString (1), rs.getString (2), rs.getString(3)));
 		}
 		
 		notifyViews ();
--- QueryApp/src/model/Query5.java
+++ QueryApp/src/model/Query5.java
@@ -12,19 +12,43 @@ public class Query5 extends QueryObject {
 	}
 	
 	private void initVariants () {
-		variants.add
-			("select title, publishername " +
-			"from book " +
-			"where publishername like '%book%'");
-			variants.add
-			("select b.title, b.publishername " +
-			"from (select title, publishername from book where publishername like '%book%') as b");
+		variants.add("SELECT BorrowerLName, b.CardNo, BranchName, BranchAddress "
+				+ "FROM book_loans bl, library_branch lb, (SELECT  "
+				+ "CardNo, BorrowerLName FROM borrower WHERE  "
+				+ "BorrowerLName = 'Hellgrove') as b  "
+				+ "WHERE lb.BranchID = bl.BranchID AND bl.CardNo = b.CardNo");
+		variants.add("SELECT BorrowerLName, b.CardNo, BranchName, BranchAddress "
+				+ "FROM book_loans bl, library_branch lb, (SELECT  "
+				+ "CardNo, BorrowerLName FROM borrower WHERE  "
+				+ "BorrowerLName = 'Hellgrove') as b  "
+				+ "WHERE lb.BranchID = bl.BranchID AND bl.CardNo = b.CardNo");
+		variants.add("SELECT BorrowerLName, b.CardNo, BranchName, BranchAddress "
+				+ "FROM book_loans bl, library_branch lb, (SELECT  "
+				+ "CardNo, BorrowerLName FROM borrower WHERE  "
+				+ "BorrowerLName = 'Hellgrove') as b  "
+				+ "WHERE lb.BranchID = bl.BranchID AND bl.CardNo = b.CardNo");
+		variants.add("SELECT BorrowerLName, b.CardNo, BranchName, BranchAddress "
+				+ "FROM book_loans bl, library_branch lb, table1 as b "
+				+ "WHERE lb.BranchID = bl.BranchID AND bl.CardNo = b.CardNo ");
 	}
 	
 	public void prepareUpdates () throws Exception {
 		table.removeAllRowItems ();
 		
 		setQuery(variants.get(viewing));
+		
+		try{dbc.executeUpdate("drop index a on book_loans");}catch(Exception e){};
+		
+		switch(viewing){
+		case 1:
+			dbc.executeUpdate("create index a on book_loans(CardNo)");
+			setQuery("create index a on book_loans(CardNo);\n" + getQuery());
+			break;
+		case 2:
+			dbc.executeUpdate("create index a on book_loans(BranchID)");
+			setQuery("create index a on book_loans(CardNo);\n" + getQuery());
+			break;
+		}
 		
 		long startTime, endTime;
 		startTime = System.nanoTime ();
