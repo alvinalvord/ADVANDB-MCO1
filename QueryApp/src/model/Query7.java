08e0fde162dc860cbfa80d591ec62c424bfecc51
alvinalvord <jasper_pillejera@dlsu.edu.ph>
2017-10-22 18:11:33 +0800

    othe queries

---
+++ QueryApp/src/model/Query7.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.sql.*;
+
+public class Query7 extends QueryObject {
+	
+	public Query7 () {
+		super ();
+		initVariants ();
+		table = new Table ("Title", "PublisherName");
+		setViewing (0);
+	}
+	
+	private void initVariants () {
+		variants.add
+			("select title, publishername " +
+			"from book " +
+			"where publishername like '%book%'");
+			variants.add
+			("select b.title, b.publishername " +
+			"from (select title, publishername from book where publishername like '%book%') as b");
+	}
+	
+	public void prepareUpdates () throws Exception {
+		table.removeAllRowItems ();
+		
+		setQuery(variants.get(viewing));
+		System.out.println (getQuery ());
+		
+		long startTime, endTime;
+		startTime = System.currentTimeMillis ();
+		ResultSet rs = dbc.executeQuery (getQuery ());
+		endTime = System.currentTimeMillis ();
+		
+		setDuration ((endTime - startTime));
+		
+		while (rs.next ()) {
+			table.addRowItem (new RowItem (rs.getString (1), rs.getString (2)));
+		}
+		
+		notifyViews ();
+	}
+	
+}
--- QueryApp/src/model/QueryFactory.java
+++ QueryApp/src/model/QueryFactory.java
@@ -3,11 +3,24 @@ package model;
 public class QueryFactory extends AbstractFactory {
 	
 	private static final Query1 q1 = new Query1 ();
+	private static final Query2 q2 = new Query2 ();
+	private static final Query3 q3 = new Query3 ();
+	private static final Query4 q4 = new Query4 ();
+	private static final Query5 q5 = new Query5 ();
+	private static final Query6 q6 = new Query6 ();
+	private static final Query7 q7 = new Query7 ();
+	private static final Query8 q8 = new Query8 ();
 	
 	public QueryObject getQueryObject (int queryNum) {
 		switch (queryNum) {
-			case 1:
-				return q1;
+			case 1: return q1;
+			case 2: return q2;
+			case 3: return q3;
+			case 4: return q4;
+			case 5: return q5;
+			case 6: return q6;
+			case 7: return q7;
+			case 8: return q8;
 		}
 		
 		return null;
